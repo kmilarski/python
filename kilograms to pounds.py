57d24cbd158db92df7db25fd37faed5a06d8cea7
Kenny Milarski <kmilarski@students.mchenry.edu>
2025-01-29 13:39:38 -0600

    Add files via upload

---
+++ kilograms to pounds.py
@@ -0,0 +1,22 @@
+# kilograms to pounds
+
+# conversion: Pounds = Kilograms * 2.20462
+conversion_rate = 2.20462
+
+# given kg value
+kg_value1 = 21
+kg_value2 = 4
+kg_value3 = 50
+kg_value4 = 32
+
+# pounds value
+lb_value1 = kg_value1 * conversion_rate
+lb_value2 = kg_value2 * conversion_rate
+lb_value3 = kg_value3 * conversion_rate
+lb_value4 = kg_value4 * conversion_rate
+
+
+print(f"{kg_value1} kilograms is {lb_value1:.2f} pounds.")
+print(f"{kg_value2} kilograms is {lb_value2:.2f} pounds.")
+print(f"{kg_value3} kilograms is {lb_value3:.2f} pounds.")
+print(f"{kg_value4} kilograms is {lb_value4:.2f} pounds.")
