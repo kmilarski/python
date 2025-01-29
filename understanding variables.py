b876d3d91f825d04746ea9e19173ff97049f32a9
Kenny Milarski <kmilarski@students.mchenry.edu>
2025-01-29 12:31:51 -0600

    Add files via upload

---
+++ understanding variables.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 10)
+
+# Integer
+t = -195
+print("Integer:", t)
+
+# Float
+h = 12.91
+print("Float:", h)
+
+# String
+firstname = "Kenny"
+print("String:", firstname)
+
+# Boolean
+is_not_active = False
+print("Boolean:", is_not_active)
