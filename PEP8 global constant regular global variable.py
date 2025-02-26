15ffb3dfb3af389b71b2014cf2684107ef034fe1
Kenny Milarski <kmilarski@students.mchenry.edu>
2025-02-26 12:29:26 -0600

    Add files via upload

---
+++ PEP8 global constant regular global variable.py
@@ -0,0 +1,15 @@
+'''Create a Python script that defines both a global constant and a regular global variable following PEP 8 conventions. Your script should:
+
+Define a constant for the speed of light(`SPEED_OF_LIGHT=299792458`).
+Define a regular global variable called `current_temperature` and assign it a value.
+Print both values.'''
+
+# Global Constants
+SPEED_OF_LIGHT = 299792458
+
+# Regular Global Variables
+current_temperature = 85
+
+# Print the values
+print("Speed of light:", SPEED_OF_LIGHT, "m/s")
+print("Current Temperature:", current_temperature, "°F")
