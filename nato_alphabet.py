9d3bfc32b9bb0e163d581ac7dc10b0230517cc70
Kenny Milarski <kmilarski@students.mchenry.edu>
2025-03-05 13:02:26 -0600

    Add files via upload

---
+++ nato_alphabet.py
@@ -0,0 +1,23 @@
+nato_alphabet = {
+    'a': 'Alfa', 'b': 'Bravo', 'c': 'Charlie', 'd': 'Delta', 'e': 'Echo',
+    'f': 'Foxtrot', 'g': 'Golf', 'h': 'Hotel', 'i': 'India', 'j': 'Juliett',
+    'k': 'Kilo', 'l': 'Lima', 'm': 'Mike', 'n': 'November', 'o': 'Oscar',
+    'p': 'Papa', 'q': 'Quebec', 'r': 'Romeo', 's': 'Sierra', 't': 'Tango',
+    'u': 'Uniform', 'v': 'Victor', 'w': 'Whiskey', 'x': 'Xray', 'y': 'Yankee', 'z': 'Zulu'
+}
+
+
+def spell(word_lower):
+    for key in word_lower:
+        if key in nato_alphabet:
+            print(nato_alphabet[key])
+
+
+def main():
+    word = input("Enter a word here: ")
+    word_lower = word.lower()
+
+    spell(word_lower)
+
+
+main()
